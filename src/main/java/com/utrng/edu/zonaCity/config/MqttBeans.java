b84a840d5c558f593718a29bfebe2db491df8f3c
Erick <erickocampocortez@gmail.com>
2024-10-14 13:26:10 -0600

    Se agrego ubicaciones ,lecionar las paradas por ruta de ida y regreso

--- src/main/java/com/utrng/edu/zonaCity/config/MqttBeans.java
+++ src/main/java/com/utrng/edu/zonaCity/config/MqttBeans.java
@@ -33,7 +33,7 @@ public class MqttBeans {
     public MqttPahoClientFactory mqttClientFactory() {
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
         MqttConnectOptions options = new MqttConnectOptions();
-        options.setServerURIs(new String[]{"tcp://192.168.0.171:1883"});
+        options.setServerURIs(new String[]{"tcp://192.168.3.162:1883"});
         options.setUserName("admin");
         String pass = "12345678";
         options.setPassword(pass.toCharArray());
